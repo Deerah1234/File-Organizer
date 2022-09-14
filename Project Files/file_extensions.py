65aa76b58be08fdcc44f7e0c38a0e9759d0b70fb
Chidera James <93952415+Deerah1234@users.noreply.github.com>
2022-09-14 13:00:33 +0100

    Create file_extensions.py

---
+++ Project Files/file_extensions.py
@@ -0,0 +1,66 @@
+__all__ = ['video_extensions', 'audio_extensions', 'image_extensions',
+           'document_extensions', 'application_extensions']
+
+video_extensions = (
+    'webm', 'mkv', 'flv', 'vob', 'ogv', 'ogg',
+    'drc', 'gif', 'gifv', 'mng', 'avi', 'mts',
+    'mts2', 'ts', 'mov', 'qt', 'wmv', 'yuv', 'rm',
+    'rmvb', 'viv', 'asf', 'amv', 'mp4', 'm4p', 'm4v',
+    'mpg', 'mp2', 'mpeg', 'mpe', 'mpv', 'mpg', 'mpeg',
+    'm2v', 'm4v', 'svi', '3gp', '3g2', 'mxf', 'roq', 'nsv',
+    'flv', 'f4v', 'f4p', 'f4a', 'f4b',
+)
+
+audio_extensions = (
+    '3gp', 'aa', 'aac', 'aax', 'act',
+    'aiff', 'alac', 'amr', 'ape', 'au',
+    'awb', 'dss', 'dvf', 'flac', 'gsm',
+    'iklax', 'ivs', 'm4a', 'm4b', 'm4p',
+    'mmf', 'mp3', 'mpc', 'msv', 'nmf', 'ogg',
+    'oga', 'mogg', 'opus', 'ra', 'rm', 'raw',
+    'rf64', 'sln', 'tta', 'voc', 'vox', 'wav',
+    'wma', 'wv', 'webm', '8svx', 'cda', 'flv', 'qcp',
+    'mpga',
+)
+
+image_extensions = (
+    'ani', 'anim', 'apng', 'art', 'avif',
+    'bmp', 'bpg', 'bsave', 'cal', 'cin', 'cpc',
+    'cpt', 'dds', 'dpx', 'ecw', 'exr', 'fits',
+    'flic', 'flif', 'fpx', 'gif', 'hdri', 'hevc',
+    'icer', 'icns', 'ico', 'cur', 'ics', 'ilbm',
+    'jbig', 'jbig2', 'jng', 'jpeg', 'jpeg-ls', 'jpeg 2000',
+    'jpeg xl', 'jpeg xr', 'jpeg xs', 'jpeg xt', 'jpeg-hdr',
+    'kra', 'mng', 'miff', 'nrrd', 'pam', 'pbm', 'pgm', 'ppm',
+    'pnm', 'pcx', 'pgf', 'pictor', 'png', 'psd', 'psb', 'psp',
+    'qoi', 'qtvr', 'ras', 'rgbe logluv tiff', 'sgi', 'tga', 'tiff',
+    'ufo', 'ufp', 'wbmp', 'webp', 'xbm', 'xcf', 'xpm', 'xwd', 'ciff',
+    'dng', 'ai', 'cdr', 'cgm', 'dxf', 'eva', 'emf', 'emf+', 'gerber',
+    'hvif', 'iges', 'pgml', 'svg', 'vml', 'wmf', 'xar', 'jpg',
+    'pjp', 'jfif', 'pjpeg',
+)
+
+document_extensions = (
+    'abw', 'acl', 'afp', 'ami', 'ans',
+    'asc', 'aww', 'ccf', 'csv', 'cwk', 'dbk',
+    'dita', 'doc', 'docm', 'docx', 'dot', 'dotx',
+    'dwd', 'egt', 'epub', 'ezw', 'fdx', 'ftm', 'ftx',
+    'gdoc', 'html', 'hwp', 'hwpml', 'log', 'lwp', 'mbp',
+    'md', 'me', 'mcw', 'mobi', 'nb', 'nbp', 'neis', 'nt',
+    'nq', 'odm', 'odoc', 'odt', 'osheet', 'ott', 'omm',
+    'pages', 'pap', 'pdax', 'pdf', 'quox', 'radix-64', 'rtf',
+    'rpt', 'sdw', 'se', 'stw', 'sxw', 'tex', 'info', 'troff',
+    'woff', 'woff2', 'pptx', 'pub', 'accdb', 'xlsx', 'txt',
+    'uof', 'uoml', 'via', 'wpd', 'wps', 'wpt', 'wrd', 'wrf',
+    'wri', 'xhtml', 'xht', 'xml', 'xps', 'otf', 'ttf',
+)
+
+application_extensions = (
+    '7z', 'zip', 'xul', 'vsd', 'tar',
+    'sh', 'rtf', 'rar', 'php', 'pdf',
+    'ogx', 'odt', 'ods', 'odp', 'mpkg',
+    'json', 'jsonld', 'jar', 'gz', 'eot',
+    'bz', 'bz2', 'cda', 'csh', 'bin', 'azw',
+    'arc', 'abw', 'zip', 'py', 'txt', 'bat',
+    'ipynb', 'mq5',
+)
